f096c633161256da6fd57e44d9c7596a6122466e
ifmatch <ifmatch@hotmail.com>
2020-05-02 19:07:47 +0200

    v0.2

--- birra.cpp
+++ birra.cpp
@@ -3,25 +3,35 @@ using namespace std;
 
 int main() {
     int averias;
-    int Jornada:
-    cout <<"cuantas averias llevas:";
+    int Jornada;
+    cout << "cuantas averias llevas:";
     cin >> averias;
     if (averias < 5) {
         cout << "Aun no." << endl;
         cout << "Sigue currando" << endl;
     }
     else {
-        cout << "Felicidades!" << endl;
+    	cout << "Que hora es?";
+	cin >> Jornada;
+		if (Jornada > 1700) {
+			cout << "Aun no has terminado" << endl;
+		 	cout << "Llama y pide Faena" << endl;
+		}
+		else {
+			cout << "Genial ya terminaste tu Jornada" << endl;
+			cout << "A POR LAS BIRRA" << endl;
+			}
+/*        cout << "Felicidades!" << endl;
         cout << "Antes de tomarte una birra, Has terminado tu jornada??" << endl;
     }
-    	cout << "Que hora es:";
+   	cout << "Que hora es:";
     	cin >> Jornada;
     if (Jornada < 1700) {
     	cout << "Aun no has terminado tu Jornada" endl;
     	cout << "Llama y pide Faena" endl;
     else {
 	cout << "Genial, ya terminaste tu Jornada" endl;
-	cout << "A POR LA BIRRA" endl;
-    }
+	cout << "A POR LA BIRRA" endl;  */
+}
  return 0;
 }
